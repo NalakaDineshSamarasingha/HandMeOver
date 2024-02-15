184838153a2680c70b8870ba19782eb6f412b9c0
NalakaDinesh <133525019+NalakaDineshSamarasingha@users.noreply.github.com>
2024-02-16 02:11:38 +0530

    responsive navbar

--- frontend/src/component/Navbar/navbar.jsx
+++ frontend/src/component/Navbar/navbar.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import './navbar.css';
 import Logo from "../../assests/testlogo.png"
 
 const Navbar = ()=> {
+    const [showMenu,setShowMenu] = useState(false);
     return(
         <section>
             <div className="navbar">
@@ -29,17 +30,32 @@ const Navbar = ()=> {
                     <div className="nav-main">
                         <div className="logo">
                             <img src={Logo} alt="Logo" style={{width:"50px"}} />
+                            <p className="category">Category</p>
                         </div>
                         <div className="btn">
                             <p className="donate-btn">Donate <br /> now</p>
                         </div>
                         <div className="mobo">
-                            <p className="menu-icon">&#9776;</p>
+                        <p className="menu-icon" onClick={() => setShowMenu(!showMenu)}>
+                            {showMenu ? <span>&#10005;</span> : <span id="click">&#9776;</span>}
+                        </p>
+
                         </div>
                     </div>
                 </div>
             </div>
             
+        </div>
+        <div className={`mobo-nav-links ${showMenu ? 'slide-in' : 'slide-out'}`} style={{display: showMenu?'flex':'none'}} >
+            <ul className="nav-links">
+                <li className="link" onClick={()=>setShowMenu(false)} >Home</li>
+                <li className="link" onClick={()=>setShowMenu(false)} >Services</li>
+                <li className="link" onClick={()=>setShowMenu(false)} >Contact</li>
+                <li className="link" onClick={()=>setShowMenu(false)} >About Us</li>
+                <li className="link" onClick={()=>setShowMenu(false)} >Login</li>
+                <li className="link" onClick={()=>setShowMenu(false)} >SignUp</li>
+                <li className="link donate" onClick={()=>setShowMenu(false)} >Donate</li>
+            </ul>
         </div>
         </section>
         
