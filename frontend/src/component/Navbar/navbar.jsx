83d2ae732fdcf60c408201b1626ceef72141c6f1
NalakaDinesh <133525019+NalakaDineshSamarasingha@users.noreply.github.com>
2024-02-15 23:31:29 +0530

    First commit

---
+++ frontend/src/component/Navbar/navbar.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import './navbar.css';
+import Logo from "../../assests/testlogo.png"
+
+const Navbar = ()=> {
+    return(
+        <div className="navbar">
+            <div className="main-container">
+                <div className="nav-main">
+                    <div className="nav-left">
+                        <ul className="nav-links">
+                            <li className="link">Home</li>
+                            <li className="link">Services</li>
+                            <li className="link">Contact</li>
+                            <li className="link">About Us</li>
+                        </ul>
+                    </div>
+                    <div className="nav-right">
+                        <ul className="nav-links">
+                            <li className="link">Log in</li>
+                            <li className="link">Sign Up</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="secoundary-container">
+                <div className="nav-secoundary">
+                    <div className="nav-main">
+                        <div className="logo">
+                            <img src={Logo} alt="Logo" style={{width:"50px"}} />
+                        </div>
+                        <div className="btn">
+                            <p className="donate-btn">Donate <br /> now</p>
+                        </div>
+                    </div>
+                </div>
+            </div> 
+        </div>
+    )
+}
+export default Navbar;
