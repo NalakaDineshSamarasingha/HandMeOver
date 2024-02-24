a41e6e88277127d69807f23d5843584dfbf11d3b
NalakaDinesh <133525019+NalakaDineshSamarasingha@users.noreply.github.com>
2024-02-24 20:52:37 +0530

    add the assistence component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './component/Header/header';
 import Navbar from './component/Navbar/navbar';
+import Assis from './component/assistence/ass';
 import Category from './component/category/category';
 import HomeSign from './component/homesignup/homesignup';
 import Intro from './component/intro/intro';
@@ -13,6 +14,7 @@ function App() {
       <Intro/>
       <HomeSign/>
       <Category/>
+      <Assis/>
     </div>
   );
 }
---
+++ frontend/src/component/assistence/ass.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import './ass.css';
+
+
+const Assis = () =>{
+    return(
+        <div className="Assis">
+            <div className="box">
+                <p className="assis-header">Hello, need assistance?</p>
+                <p className="ab">We understand that buying and selling a digital business isn't easy. If you have any questions or require assistance, feel free to contact us anytime.</p>
+                <div className="assis-btn">
+                    <span className="guildeline">Read our guidelines</span>
+                    <span className="contact">Contact Us</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default Assis;
